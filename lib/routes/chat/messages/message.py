cf3c7e78cfe2649990e99b3c8406d0b9701418aa
windownik <windownik@gmail.com>
2023-07-13 10:05:15 +0300

    work with messages socket api

--- lib/routes/chat/messages/message.py
+++ lib/routes/chat/messages/message.py
@@ -1,5 +1,6 @@
 import json
 
+from starlette.websockets import WebSocketDisconnect
 from fastapi import WebSocket, Depends
 from fastapi.responses import HTMLResponse
 
@@ -63,6 +64,7 @@ async def websocket_endpoint(websocket: WebSocket, user_id: int, db=Depends(data
 
             if not check:
                 continue
-
+    except WebSocketDisconnect:
+        manager.disconnect(user_id=user_id)
     except Exception as ex:
         print(ex)
