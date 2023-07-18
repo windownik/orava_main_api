35b5b4194f5af5d3cb332306cdb8ecba93ca39e5
windownik <windownik@gmail.com>
2023-07-18 12:29:03 +0300

    add msg_server_id

--- lib/routes/chat/messages/message.py
+++ lib/routes/chat/messages/message.py
@@ -71,6 +71,7 @@ async def websocket_endpoint(websocket: WebSocket, user_id: int, db=Depends(data
             data = json.loads(data)
             if 'echo' in data.keys():
                 data["user_id"] = user_id
+                await manager.connect(websocket, user_id=user_id)
                 await websocket.send_json(str(data))
                 continue
             check = await msg_manager(data, db=db, user_id=user_id, websocket=websocket, manager=manager)
